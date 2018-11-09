656dd0560ed7e518a1205450398b8fb55a53e875
Brynn Shepherd <brynn.shepherd@gmail.com>
2018-11-09 12:32:42 -0500

    database schemas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 const app = express();
 const morgan = require('morgan');
 const router = require('./routes/index');
-const { db } = require('./models');
+const models = require('./models');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -14,11 +14,18 @@ module.exports = app; // this line is only used to make testing easier.
 app.use(express.urlencoded({ extended: false }));
 app.use('/', router);
 
-db.authenticate().then(() => {
-  console.log('connected to the database');
-});
+const PORT = 1234;
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+const init = async () => {
+  try {
+    await models.db.sync();
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+};
+
+init();
